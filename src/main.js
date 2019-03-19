1c0bb6a2db504c81fe6bc98d1a78201c90d4f232
Student <example@example.com>
2019-03-19 10:12:09 -0700

    install dotenv-webpack

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
-// import $ from 'jquery';
-  import 'bootstrap';
+import $ from 'jquery';
+  // import 'bootstrap';
   import 'bootstrap/dist/css/bootstrap.min.css';
   // import './css/styles.css';s
-  import './sass/styles.scss';
+  // import './sass/styles.scss';
+
+  $ (document).ready(function(){
+    $('#weatherLocation').click(function() {
+      let city = $('#location').val();
+      let secretKey = process.env.API_KEY;
+      $('#location').val("");
+      $.ajax({
+        url:`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${secretKey}`,
+        type: 'GET',
+        data: {
+          format: 'json'
+        },
+        success: function(response) {
+          $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+          $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+          $('.showPressure').text(`The pressure in ${city} is ${response.main.pressure}`);
+        },
+        error: function(response) {
+          $('#errors').text("There was an error processing your request. Please try again.");
+        }
+      });
+    });
+  });
