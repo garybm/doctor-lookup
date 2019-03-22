d09f704e6c88fd5c3d239c3989f6b1e3f5bc66d3
Gary Bermudez <garybm.17@gmail.com>
2019-03-22 10:24:13 -0700

    fix the error with the DoctorLooup class

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -1,10 +1,12 @@
 //BUSSINESS LOGIC
 
-  export class DoctorLookUp {
-    getWeatherByCity(zipCode) {
+  export class DoctorLookup {
+    getDoctorByIssue(condition) {
       return new Promise(function(resolve, reject) {
         let request = new XMLHttpRequest();
-        let url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us &appid=process.env.apiKey`;
+        console.log(request);
+        let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=2a0c7f757f668f9f1bd09c9363d3a261';
+        console.log(url);
         request.onload = function() {
           if (this.status === 200) {
             resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -1,25 +1,23 @@
 
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { DoctorLookUp } from './../src/doctor-lookup.js';
+import { DoctorLookup } from './../src/doctor-lookup.js';
 
 
 $(document).ready(function() {
-$('#weatherLocation').click(function() {
-  let zipCode = $('#location').val();
-  $('#location').val("");
+$('#findDoctor').click(function() {
+  let condition = $('#condition').val();
+  $('#condition').val("");
+  console.log(condition);
 
 
-  let docotrLookup = new DoctorLookUp();  // create instance of WeatherService class
-  let promise = weatherService.getDocotorByIssue(issue);  // call the instance method and pass in user input
+  let doctorLookup = new DoctorLookup();  // create instance of WeatherService class
+  let promise = doctorLookup.getDoctorByIssue(condition);  // call the instance method and pass in user input
 
   promise.then(function(response) {
     let body = JSON.parse(response);
-    $('.showHumidity').text(`The humidity in ${body.name} is ${body.main.humidity}%`);
-    $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-    $('.showWind').text(`The wind speed is ${body.wind.speed} `);
-    $('.showName').text(`The best city ${body.name}`);
-  }, function(error) {
+    $('.doctorName').text(`Doctor name: ${body.doctors} is`);
+    }, function(error) {
     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
   });
 });
