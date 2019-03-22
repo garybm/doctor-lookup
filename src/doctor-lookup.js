0a7050348934645e00a8145293f7ea6ffb235bfb
Gary Bermudez <garybm.17@gmail.com>
2019-03-22 12:13:54 -0700

    add a for loop

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -5,8 +5,7 @@
       return new Promise(function(resolve, reject) {
         let request = new XMLHttpRequest();
         console.log(request);
-        let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=2a0c7f757f668f9f1bd09c9363d3a261';
-        console.log(url);
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=2a0c7f757f668f9f1bd09c9363d3a261`;
         request.onload = function() {
           if (this.status === 200) {
             resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -8,18 +8,22 @@ $(document).ready(function() {
 $('#findDoctor').click(function() {
   let condition = $('#condition').val();
   $('#condition').val("");
-  console.log(condition);
 
 
-  let doctorLookup = new DoctorLookup();  // create instance of WeatherService class
-  let promise = doctorLookup.getDoctorByIssue(condition);  // call the instance method and pass in user input
+  let doctorLookup = new DoctorLookup();
+  let promise = doctorLookup.getDoctorByIssue(condition);
 
   promise.then(function(response) {
     let body = JSON.parse(response);
-    $('.doctorName').text(`Doctor name: ${body.doctors} is`);
+    console.log(body);
+    body.data.forEach(function(index){
+          for (let i = 0; i < index.specialties.length; i++) {
+           if (index.specialties[i].description.match(condition)) {
+             let firstName = index.profile.first_name;
+             $(".printInfo").append("<p> First Name: " + firstName + "</p>");
     }, function(error) {
-    $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-  });
-});
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    })
 
+  });
 });
