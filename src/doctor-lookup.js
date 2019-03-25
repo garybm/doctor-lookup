1b7688eab2ea37aff5d4d6f46924b9128c99e963
Gary Bermudez <garybm.17@gmail.com>
2019-03-24 18:57:03 -0700

    add a class to create an instance Doctor, allow to print 10 results in the page

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -5,7 +5,7 @@
       return new Promise(function(resolve, reject) {
         let request = new XMLHttpRequest();
         let apiKey = process.env.apiKey;
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=OR&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=` + apiKey + '&query=' + condition ;
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=OR&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=` + apiKey + '&query=' + condition;
         console.log(url);
         request.onload = function() {
           if (this.status === 200) {
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { DoctorLookup } from './../src/doctor-lookup.js';
+import { Doctor } from './../src/new-doctor.js';
 import './sass/styles.scss';
 
 
@@ -9,42 +10,34 @@ $(document).ready(function() {
   $('#findDoctor').click(function() {
     let condition = $('#condition').val();
     $('#condition').val("");
-
-
     let doctorLookup = new DoctorLookup();
-
     let promise = doctorLookup.getDoctorByIssue(condition);
 
     promise.then(function(response) {
       let doctorResponse = JSON.parse(response);
         doctorResponse.data.forEach(doctor => {
+
           $("#results").show();
-          $('.firstName').text(`Doctor's name: ${doctor.profile.first_name} ${doctor.profile.last_name}`);
-
+          let name = doctor.profile.first_name + " " + doctor.profile.last_name;
           let acceptNew = doctor.practices[0].accepts_new_patients;
-            if (acceptNew  === true) {
-              $(".newPat").text(" Accepting new patients: YES!");
-            } else {
-              $(".newPat").text("Accepting new patients: NO");
-            }
-            let addressStreet = doctor.practices[0].visit_address.street;
-            let addressCity = doctor.practices[0].visit_address.city;
-            let zip = doctor.practices[0].visit_address.zip;
-           $(".address").text("Practice Location: " + addressStreet);
-           $(".addressCity").text( addressCity + " " + zip);
+          // if (acceptNew === true) {
+          //   return "This doctor is accepting new patients!"
+          // } else {
+          //   return "This doctor is not accepting new patients!"
+          // }
+          let addressStreet = doctor.practices[0].visit_address.street;
+          let addressCity = doctor.practices[0].visit_address.city + ', '  + doctor.practices[0].visit_address.state + ' ' + doctor.practices[0].visit_address.zip;
+          let phone = doctor.practices[0].phones[0].number;
+          let website = doctor.practices[0].website;
 
+          //NEW Doctor
+          let newDoctor = new Doctor(name, acceptNew, addressStreet, addressCity, phone, website);
 
+           $('#results').append(newDoctor.toHTML());
 
-           // let phoneNumber = doctor.practices[0].phone;
-           // console.log(phoneNumber),
-
-      });
-
-         $("#results").show();
-
-      }, function(error) {
-        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-      });
-
-  });
-});
+         }, function(error) {
+           $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+         });
+       });
+     });
+     });
